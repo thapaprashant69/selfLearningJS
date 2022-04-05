560b33bcc1904641b6b08cb313efc23ab9ea0646
unknown <thapaprashant69@gmail.com>
2022-04-05 11:15:54 +0545

    FizzBuzz game from Eloquent JS book added

---
+++ fizzBuzz.js
@@ -0,0 +1,15 @@
+for(let i=1;i<=100;i++){
+    if(i%3==0){
+        if(i%5==0){
+            console.log("FizzBuzz");
+            continue;
+        }
+        console.log("Fizz");
+    }
+    else if(i%5==0){
+        console.log("Buzz");
+    }
+    else{
+        console.log(i);
+    }
+}
